8227a040da4eed1fcedc2ed290bf049b045fadd3
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-08-10 11:14:48 +0530

    Update RegionsCutBySlashes.cpp

--- Leetcode_Problem/RegionsCutBySlashes.cpp
+++ Leetcode_Problem/RegionsCutBySlashes.cpp
@@ -1,8 +1,6 @@
 /*
  Regions Cut By Slashes
-Medium
-Topics
-Companies
+
 An n x n grid is composed of 1 x 1 squares where each 1 x 1 square consists of a '/', '\', or blank space ' '. These characters divide the square into contiguous regions.
 
 Given the grid grid represented as a string array, return the number of regions.
