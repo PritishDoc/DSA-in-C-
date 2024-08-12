51bdb19820d64eb42b8ceb48f0ff2bac9367aa14
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-08-12 11:23:15 +0530

    Create Kth_Largest_Element.cpp

---
+++ Leetcode_Problem/Kth_Largest_Element.cpp
@@ -0,0 +1,62 @@
+/*
+Kth Largest Element in a Stream
+Solved
+Easy
+Topics
+Companies
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Implement KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+ 
+
+Example 1:
+
+Input
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+Output
+[null, 4, 5, 5, 8, 8]
+
+Explanation
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3);   // return 4
+kthLargest.add(5);   // return 5
+kthLargest.add(10);  // return 5
+kthLargest.add(9);   // return 8
+kthLargest.add(4);   // return 8
+*/
+
+class KthLargest {
+private:
+    int k;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;  // Min-heap to store the k largest elements
+
+public:
+    KthLargest(int k, std::vector<int>& nums) : k(k) {
+        for (int num : nums) {
+            add(num);  // Add initial elements to the heap
+        }
+    }
+    
+    int add(int val) {
+        // Add the new value to the heap
+        minHeap.push(val);
+        
+        // If the heap size exceeds k, remove the smallest element
+        if (minHeap.size() > k) {
+            minHeap.pop();
+        }
+        
+        // The root of the heap is the kth largest element
+        return minHeap.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
