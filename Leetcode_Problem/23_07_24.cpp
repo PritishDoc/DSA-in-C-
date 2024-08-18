6211ff95632f12bfbf99f50d615e51fd5ec553db
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-08-18 22:24:28 +0530

    Update 23_07_24.cpp

--- Leetcode_Problem/23_07_24.cpp
+++ Leetcode_Problem/23_07_24.cpp
@@ -1,9 +1,5 @@
 /*
 Sort Array by Increasing Frequency
-Solved
-Easy
-Topics
-Companies
 Hint
 Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
 
