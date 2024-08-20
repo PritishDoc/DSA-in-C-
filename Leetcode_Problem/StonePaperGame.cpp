8167b1ecfc9dc3c36e7afa640ed8523964e75498
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-08-20 21:35:28 +0530

    Update StonePaperGame.cpp

--- Leetcode_Problem/StonePaperGame.cpp
+++ Leetcode_Problem/StonePaperGame.cpp
@@ -28,3 +28,27 @@ Output: 104
  
 */
 
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        vector<int> suffixSum(n + 1, 0);
+
+        // Calculate the suffix sum array
+        for (int i = n - 1; i >= 0; --i) {
+            suffixSum[i] = suffixSum[i + 1] + piles[i];
+        }
+
+        // Fill the dp table
+        for (int i = n - 1; i >= 0; --i) {
+            for (int m = 1; m <= n; ++m) {
+                for (int x = 1; x <= 2 * m && i + x <= n; ++x) {
+                    dp[i][m] = max(dp[i][m], suffixSum[i] - dp[i + x][max(m, x)]);
+                }
+            }
+        }
+
+        return dp[0][1];
+    }
+};
