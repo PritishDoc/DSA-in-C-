9b3a317c5a45bc1d27d2360546f204e4d7546a52
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-08-21 09:13:39 +0530

    Update 26_07_24.cpp

--- Leetcode_Problem/26_07_24.cpp
+++ Leetcode_Problem/26_07_24.cpp
@@ -1,9 +1,5 @@
 /*
 Find the City With the Smallest Number of Neighbors at a Threshold Distance
-Solved
-Medium
-Topics
-Companies
 Hint
 There are n cities numbered from 0 to n-1. Given the array edges where edges[i] = [fromi, toi, weighti] represents a bidirectional and weighted edge between cities fromi and toi, and given the integer distanceThreshold.
 
