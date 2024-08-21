7b81b63409fe15c70769749115d3d76688d65680
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-08-21 09:06:02 +0530

    Update StrangePrinter.cpp

--- Leetcode_Problem/StrangePrinter.cpp
+++ Leetcode_Problem/StrangePrinter.cpp
@@ -29,3 +29,24 @@ Constraints:
 1 <= s.length <= 100
 s consists of lowercase English letters.
 */
+class Solution {
+public:
+    int strangePrinter(string s) {
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i][i] = 1;  // Base case: A single character needs 1 turn
+            for (int j = i + 1; j < n; ++j) {
+                dp[i][j] = dp[i][j - 1] + 1;  // Start with the worst case: print s[i] to s[j-1] and then s[j]
+                for (int k = i; k < j; ++k) {
+                    if (s[k] == s[j]) {
+                        dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j - 1]);
+                    }
+                }
+            }
+        }
+        
+        return dp[0][n - 1];
+    }
+};
