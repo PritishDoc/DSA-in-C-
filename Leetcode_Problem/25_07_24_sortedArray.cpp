0fb096c27a24cddd04bf83fcee36408022405b74
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-08-22 20:54:57 +0530

    Update 25_07_24_sortedArray.cpp

--- Leetcode_Problem/25_07_24_sortedArray.cpp
+++ Leetcode_Problem/25_07_24_sortedArray.cpp
@@ -1,10 +1,7 @@
 
 /*
  Sort an Array
-Solved
-Medium
-Topics
-Companies
+
 Given an array of integers nums, sort the array in ascending order and return it.
 
 You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
