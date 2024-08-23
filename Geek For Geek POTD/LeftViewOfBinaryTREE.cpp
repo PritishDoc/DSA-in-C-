0b1e7fdcd00b6e787a22d319bbf829201439438a
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-08-23 21:50:53 +0530

    Update LeftViewOfBinaryTREE.cpp

--- Geek For Geek POTD/LeftViewOfBinaryTREE.cpp
+++ Geek For Geek POTD/LeftViewOfBinaryTREE.cpp
@@ -1,3 +1,36 @@
+/*
+Left View of Binary Tree
+Difficulty: EasyAccuracy: 33.74%Submissions: 508K+Points: 2
+Given a Binary Tree, return Left view of it. Left view of a Binary Tree is set of nodes visible when tree is visited from Left side. The task is to complete the function leftView(), which accepts root of the tree as argument. If no left view is possible, return an empty tree.
+
+Left view of following tree is 1 2 4 8.
+
+          1
+       /     \
+     2        3
+   /     \    /    \
+  4     5   6    7
+   \
+     8   
+
+Examples :
+
+Input:
+   1
+ /  \
+3    2
+Output: 1 3
+
+Input:
+
+Output: 10 20 40
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(N).
+
+Constraints:
+0 <= Number of nodes <= 105
+0 <= Data of a node <= 105
+*/
 vector<int> leftView(Node *root) {
     vector<int> result;
     if (!root) return result;  // If tree is empty, return an empty list
