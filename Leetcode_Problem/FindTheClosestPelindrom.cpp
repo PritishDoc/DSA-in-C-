1d248288d45f414863ab44ca965f1d79db73460f
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-08-24 19:52:15 +0530

    Update FindTheClosestPelindrom.cpp

--- Leetcode_Problem/FindTheClosestPelindrom.cpp
+++ Leetcode_Problem/FindTheClosestPelindrom.cpp
@@ -1,13 +1,7 @@
 /*
 
-Code
-Testcase
-Test Result
-Test Result
 564. Find the Closest Palindrome
-Hard
-Topics
-Companies
+
 Hint
 Given a string n representing an integer, return the closest integer (not including itself), which is a palindrome. If there is a tie, return the smaller one.
 
