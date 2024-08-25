3b941de1cc63a5a718f501c2593c7f1a69f3bb56
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-08-25 21:59:29 +0530

    Update BinaryTreePostOrderTraversal.cpp

--- Leetcode_Problem/BinaryTreePostOrderTraversal.cpp
+++ Leetcode_Problem/BinaryTreePostOrderTraversal.cpp
@@ -28,3 +28,19 @@ The number of the nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
  
 */
+class Solution {
+public:
+    void postorder(TreeNode* root, vector<int>& result) {
+        if (root == nullptr) return; // Base case: if the node is null, return
+        
+        postorder(root->left, result);  // Traverse the left subtree
+        postorder(root->right, result); // Traverse the right subtree
+        result.push_back(root->val);    // Visit the root node
+    }
+    
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result; // This will store the postorder traversal
+        postorder(root, result);
+        return result;
+    }
+};
