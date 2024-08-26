9d9cb1c0f128341def2b4a04eeaf0a8183aeb38f
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-08-26 21:35:52 +0530

    Update N_aryTreePostorderTraversal.cpp

--- Leetcode_Problem/N_aryTreePostorderTraversal.cpp
+++ Leetcode_Problem/N_aryTreePostorderTraversal.cpp
@@ -1,8 +1,5 @@
 /*
 590. N-ary Tree Postorder Traversal
-Easy
-Topics
-Companies
 Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
 
 Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
