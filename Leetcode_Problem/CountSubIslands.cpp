3334afba24f8c3adb57b4ef9a1f5235200f14aef
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-08-28 20:47:59 +0530

    Update CountSubIslands.cpp

--- Leetcode_Problem/CountSubIslands.cpp
+++ Leetcode_Problem/CountSubIslands.cpp
@@ -37,3 +37,49 @@ grid1[i][j] and grid2[i][j] are either 0 or 1.
 
 */
 
+class Solution {
+public:
+    // DFS function to traverse the grid
+    bool dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j) {
+        // Check if the indices are out of bounds or if the cell is water (0)
+        if (i < 0 || j < 0 || i >= grid1.size() || j >= grid1[0].size() || grid2[i][j] == 0) {
+            return true;
+        }
+        
+        // If the cell in grid2 is land but grid1 has water, this island in grid2 is not a sub-island
+        if (grid1[i][j] == 0) {
+            return false;
+        }
+
+        // Mark the cell in grid2 as visited by setting it to 0
+        grid2[i][j] = 0;
+
+        // Explore all 4 directions (up, down, left, right)
+        bool up = dfs(grid1, grid2, i-1, j);
+        bool down = dfs(grid1, grid2, i+1, j);
+        bool left = dfs(grid1, grid2, i, j-1);
+        bool right = dfs(grid1, grid2, i, j+1);
+        
+        // If all directions are part of a sub-island, return true
+        return up && down && left && right;
+    }
+
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        int count = 0;
+        
+        // Iterate over every cell in grid2
+        for (int i = 0; i < grid2.size(); i++) {
+            for (int j = 0; j < grid2[0].size(); j++) {
+                // If we find a land cell in grid2, check if it's a sub-island
+                if (grid2[i][j] == 1) {
+                    // If DFS returns true, it's a sub-island
+                    if (dfs(grid1, grid2, i, j)) {
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+};
