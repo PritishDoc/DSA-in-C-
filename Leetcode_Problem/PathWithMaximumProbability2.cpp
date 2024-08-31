c67292e1525fa6d0a5b4977563f57c442a02aea5
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-08-31 21:56:19 +0530

    Update PathWithMaximumProbability2.cpp

--- Leetcode_Problem/PathWithMaximumProbability2.cpp
+++ Leetcode_Problem/PathWithMaximumProbability2.cpp
@@ -1,8 +1,5 @@
 /*
 Path with Maximum Probability
-Solved
-Medium
-Topics
 Companies
 Hint
 You are given an undirected weighted graph of n nodes (0-indexed), represented by an edge list where edges[i] = [a, b] is an undirected edge connecting the nodes a and b with a probability of success of traversing that edge succProb[i].
