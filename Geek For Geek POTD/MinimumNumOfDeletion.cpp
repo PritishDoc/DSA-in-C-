14875e9cd699f4050eb029819ea59c0c923f4506
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-09-03 17:14:50 +0530

    Update MinimumNumOfDeletion.cpp

--- Geek For Geek POTD/MinimumNumOfDeletion.cpp
+++ Geek For Geek POTD/MinimumNumOfDeletion.cpp
@@ -22,3 +22,33 @@ Constraints:
 All the characters are lowercase English alphabets
 
 */
+class Solution {
+public:
+    int minOperations(string str1, string str2) {
+        int m = str1.size();
+        int n = str2.size();
+        
+        // Create a 2D DP array to store the length of LCS
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        
+        // Fill the DP array
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (str1[i-1] == str2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        // Length of LCS
+        int LCS_length = dp[m][n];
+        
+        // Calculate the minimum number of operations
+        int deletions = m - LCS_length;
+        int insertions = n - LCS_length;
+        
+        return deletions + insertions;
+    }
+};
