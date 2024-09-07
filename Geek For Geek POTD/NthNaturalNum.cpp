61cb708028a54fa034fc85b3b3ffb2a5e5df20c6
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-09-07 19:21:36 +0530

    Update NthNaturalNum.cpp

--- Geek For Geek POTD/NthNaturalNum.cpp
+++ Geek For Geek POTD/NthNaturalNum.cpp
@@ -19,3 +19,19 @@ Constraints:
 1 ≤ n ≤ 109
 
 */
+
+class Solution {
+public:
+    long long findNth(long long n) {
+        long long result = 0;
+        long long base = 1;
+
+        while (n > 0) {
+            result += (n % 9) * base;
+            n /= 9;
+            base *= 10;
+        }
+
+        return result;
+    }
+};
