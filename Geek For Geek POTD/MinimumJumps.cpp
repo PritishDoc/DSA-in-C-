8d7eeb5bc7d25a25250844db32ef037b4f6b2dd4
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-09-08 16:45:47 +0530

    Update MinimumJumps.cpp

--- Geek For Geek POTD/MinimumJumps.cpp
+++ Geek For Geek POTD/MinimumJumps.cpp
@@ -23,3 +23,40 @@ Constraints:
 0 ≤ arr[i] ≤ 105
 2 ≤ arr.size() ≤ 106
 */
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        // If the array size is 1, we're already at the end
+        if (n == 1) return 0;
+        // If the first element is 0, we can't move forward
+        if (arr[0] == 0) return -1;
+
+        int jumps = 1; // At least one jump is needed
+        int farthest = arr[0];
+        int current_end = arr[0];
+
+        for (int i = 1; i < n; ++i) {
+            // If we've reached the last element
+            if (i == n - 1) return jumps;
+
+            // Update the farthest reachable index
+            farthest = max(farthest, i + arr[i]);
+
+            // If we've reached the end of the current jump range
+            if (i == current_end) {
+                // We need to make another jump
+                jumps++;
+
+                // Update the current jump range to the farthest we've found
+                current_end = farthest;
+
+                // If the current_end is still less than or equal to i, 
+                // it means we can't move forward
+                if (current_end <= i) return -1;
+            }
+        }
+        
+        return -1;
+    }
+};
