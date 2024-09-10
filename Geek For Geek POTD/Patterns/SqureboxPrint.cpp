5769224e0fad99bd9b90fd5f96eb4eb2c1e89d84
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-09-10 17:43:46 +0530

    Patterns question

---
+++ Geek For Geek POTD/Patterns/SqureboxPrint.cpp
@@ -0,0 +1,29 @@
+/*
+Print thhe Partten of 1234 like a squre or print ABCD same way 
+01234
+01234
+01234
+01234
+
+or
+
+ABCDE
+ABCDE
+ABCDE
+ABCDE
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    char ch='A';
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<j;
+           // cout<<ch;
+            //ch++;
+        }
+        cout<<endl;
+    }
+}
