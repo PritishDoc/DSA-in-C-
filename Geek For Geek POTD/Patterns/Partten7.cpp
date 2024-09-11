0dc885b19950d76cb4ed86fa0edcaa030534b3b9
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-09-11 15:47:52 +0530

    REVERSE tRIENGLE

---
+++ Geek For Geek POTD/Patterns/Partten7.cpp
@@ -0,0 +1,29 @@
+/*
+
+11111
+ 2222
+  333
+   44
+    5
+    
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n=5;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<" ";
+
+        }
+        for(int j=0;j<n-i;j++){
+            cout<<i+1;
+        }
+        cout<<endl;
+
+    }
+    cout<<endl;
+}
