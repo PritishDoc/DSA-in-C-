921c9e9e01c6918d3836c6dc58c5f31f96246d5d
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-09-11 16:02:43 +0530

    Pyramid pATTERN

---
+++ Geek For Geek POTD/Patterns/Partten8.cpp
@@ -0,0 +1,30 @@
+/*
+   1
+   121
+  12321
+ 1234321
+123454321
+
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n=5;
+    for(int i=0;i<n;i++){
+        //Now we printing space : n-i-1
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        //Now Printing the the number1
+        for(int j=1;j<=i+1;j++){
+            cout<<j;
+        
+        }
+        for(int j=i;j>0;j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
