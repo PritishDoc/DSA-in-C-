df3e85a6b2b2e61bf93719794ae497dc6159f30a
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-09-13 11:30:25 +0530

    ll

--- Array/A1.cpp
+++ Array/A1.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 
 int main(){
-    int marks[100];
+    int marks[5]={33,99,89,46,66};
 
+    double price[]={88.99,44.88,44.90,56.08};
+
+    for(int i=0;i<sizeof(marks);i++){
+            cout<<marks[i]<<endl;
+    }
+    //To Calculate the size of the Array
+    cout<<sizeof(marks)/sizeof(int)<<endl;
   
 
 }
