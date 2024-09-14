483e76a8687f92fa58f4fe025b7cf72eda738f58
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-09-14 11:21:14 +0530

    k

--- Array/LinearSearch.cpp
+++ Array/LinearSearch.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
-
+int LinearSearch(int arr[],int size,int target){
+ for(int i=0;i<size;i++){
+        if(arr[i]==target){
+            cout<<"The Target Element is : "<<i;
+        }
+       
+    }
+    return -1;
+}
 int main(){
-    
+    int arr[]={4,5,6,3,9,1};
+    int size=6;
+    int target=11;
+    cout<<LinearSearch(arr,size,target)<<endl;
 }
