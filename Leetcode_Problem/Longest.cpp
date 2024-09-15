30b30ef68342fc703f6fe9ff935fb2f7df1d6cf6
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-09-15 21:11:10 +0530

    Update Longest.cpp

--- Leetcode_Problem/Longest.cpp
+++ Leetcode_Problem/Longest.cpp
@@ -31,3 +31,38 @@ Constraints:
 1 <= s.length <= 5 x 10^5
 s contains only lowercase English letters.
 */
+class Solution {
+public:
+    int findTheLongestSubstring(string s) {
+        unordered_map<int, int> maskPos;
+        maskPos[0] = -1;  // Initial state: all vowels occur an even number of times
+        int mask = 0;  // This will be used to track the parity of vowels
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s[i];
+            // Update the mask for the current vowel
+            if (ch == 'a') {
+                mask ^= 1 << 0;  // Flip the bit for 'a'
+            } else if (ch == 'e') {
+                mask ^= 1 << 1;  // Flip the bit for 'e'
+            } else if (ch == 'i') {
+                mask ^= 1 << 2;  // Flip the bit for 'i'
+            } else if (ch == 'o') {
+                mask ^= 1 << 3;  // Flip the bit for 'o'
+            } else if (ch == 'u') {
+                mask ^= 1 << 4;  // Flip the bit for 'u'
+            }
+
+            // If the current mask has been seen before, calculate the substring length
+            if (maskPos.find(mask) != maskPos.end()) {
+                maxLength = max(maxLength, i - maskPos[mask]);
+            } else {
+                // Otherwise, store the position of this mask
+                maskPos[mask] = i;
+            }
+        }
+
+        return maxLength;
+    }
+};
