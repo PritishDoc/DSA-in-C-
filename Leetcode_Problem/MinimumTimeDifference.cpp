5d9e442347c8b62b2f495f05578e9f753e4719f9
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-09-16 21:23:59 +0530

    Update MinimumTimeDifference.cpp

--- Leetcode_Problem/MinimumTimeDifference.cpp
+++ Leetcode_Problem/MinimumTimeDifference.cpp
@@ -1,6 +1,5 @@
 /*
  Minimum Time Difference
-Medium
 
 Given a list of 24-hour clock time points in "HH:MM" format, return the minimum minutes difference between any two time-points in the list.
  
