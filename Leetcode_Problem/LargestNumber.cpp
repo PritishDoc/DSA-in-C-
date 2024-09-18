85a998d301e99e28db8285993d89dfb87894237c
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-09-18 17:37:17 +0530

    Update LargestNumber.cpp

--- Leetcode_Problem/LargestNumber.cpp
+++ Leetcode_Problem/LargestNumber.cpp
@@ -1,8 +1,6 @@
 /*
 Largest Number
-Medium
-Topics
-Companies
+
 Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
 
 Since the result may be very large, so you need to return a string instead of an integer.
