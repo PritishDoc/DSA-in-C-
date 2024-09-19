c7417cb2fd554c8b721368afbc982655059b51cc
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-09-19 17:35:08 +0530

    Update DifferentwayToaddParanthesis.cpp

--- Leetcode_Problem/DifferentwayToaddParanthesis.cpp
+++ Leetcode_Problem/DifferentwayToaddParanthesis.cpp
@@ -33,3 +33,61 @@ expression consists of digits and the operator '+', '-', and '*'.
 All the integer values in the input expression are in the range [0, 99].
 The integer values in the input expression do not have a leading '-' or '+' denoting the sign.
 */
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    // Memoization cache to store previously computed results
+    unordered_map<string, vector<int>> memo;
+    
+    vector<int> diffWaysToCompute(string expression) {
+        // If the expression result is already computed, return it
+        if (memo.find(expression) != memo.end()) {
+            return memo[expression];
+        }
+        
+        vector<int> result;
+        
+        // Loop through each character in the expression
+        for (int i = 0; i < expression.size(); i++) {
+            char c = expression[i];
+            
+            // If the character is an operator
+            if (c == '+' || c == '-' || c == '*') {
+                // Split the expression into left and right parts
+                string left = expression.substr(0, i);
+                string right = expression.substr(i + 1);
+                
+                // Recursively calculate all possible results for left and right parts
+                vector<int> leftResults = diffWaysToCompute(left);
+                vector<int> rightResults = diffWaysToCompute(right);
+                
+                // Combine the results from the left and right parts based on the operator
+                for (int leftVal : leftResults) {
+                    for (int rightVal : rightResults) {
+                        if (c == '+') {
+                            result.push_back(leftVal + rightVal);
+                        } else if (c == '-') {
+                            result.push_back(leftVal - rightVal);
+                        } else if (c == '*') {
+                            result.push_back(leftVal * rightVal);
+                        }
+                    }
+                }
+            }
+        }
+        
+        // If no operator was found, the expression is a single number
+        if (result.empty()) {
+            result.push_back(stoi(expression));
+        }
+        
+        // Store the result in the memoization cache
+        memo[expression] = result;
+        return result;
+    }
+};
