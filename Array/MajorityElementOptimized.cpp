ecbb04cb10e5c4b14cfd5e1c3777a3471fa85882
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-09-20 19:00:41 +0530

    s

--- Array/MajorityElementOptimized.cpp
+++ Array/MajorityElementOptimized.cpp
@@ -1,31 +1,30 @@
 #include<iostream>
+#include<algorithm> // for sort
 using namespace std;
-//Optimize approch  Approch
+
 int main(){
-    int n=5;
-    int arr[5]={1,1,2,2,2};
+    int n = 5;
+    int arr[5] = {1, 1, 2, 2, 2};
 
-        //First Sort the array
-        sort(arr.begin(),arr.end());
+    // Sort the array
+    sort(arr, arr + n);
 
-        //freq count
-        int freq=1;
-        int ans=arr[0];
+    // Frequency count
+    int freq = 1, maxFreq = 1, ans = arr[0];
 
-        for(int i=0;i<n;i++){
-            if(nums[i]==nums[i-1]){
-                freq++;
-            }
-            else{
-                freq=1;
-                ans=nums[i];
-            }
+    for (int i = 1; i < n; i++) {
+        if (arr[i] == arr[i - 1]) {
+            freq++;
+        } else {
+            freq = 1; // reset frequency count
         }
-            
+        if (freq > maxFreq) { // update the answer if higher frequency is found
+            maxFreq = freq;
+            ans = arr[i];
         }
-        
+    }
     
-
+    cout << "Most frequent element is: " << ans << endl;
     
-
-//T(n)=
+    return 0;
+}
