9fcfb342a4a7c06515dd21ea1198c71a4721595b
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-09-23 22:06:29 +0530

    Update ExtraCharacterString.cpp

--- Leetcode_Problem/ExtraCharacterString.cpp
+++ Leetcode_Problem/ExtraCharacterString.cpp
@@ -2,7 +2,7 @@
 Extra Characters in a String
 Medium
 Topics
-Companies
+
 Hint
 You are given a 0-indexed string s and a dictionary of words dictionary. You have to break s into one or more non-overlapping substrings such that each substring is present in dictionary. There may be some extra characters in s which are not present in any of the substrings.
 
