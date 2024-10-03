bd8cfe78ba227c722991f1733b0960de0ad198e6
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-10-03 21:49:20 +0530

    Update MajorityVote.cpp

--- Geek For Geek POTD/MajorityVote.cpp
+++ Geek For Geek POTD/MajorityVote.cpp
@@ -21,3 +21,48 @@ Constraint:
 0 <= nums[i] <= 109
 
 */
+
+class Solution {
+public:
+    vector<int> findMajority(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return {-1};
+        
+        // Step 1: Identify two candidates using Boyer-Moore Voting Algorithm
+        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
+        
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        // Step 2: Verify if the candidates appear more than n/3 times
+        count1 = count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+        
+        vector<int> result;
+        if (count1 > n / 3) result.push_back(candidate1);
+        if (count2 > n / 3) result.push_back(candidate2);
+        
+        if (result.empty()) return {-1};
+        
+        // Step 3: Return result in increasing order
+        sort(result.begin(), result.end());
+        return result;
+    }
+};
