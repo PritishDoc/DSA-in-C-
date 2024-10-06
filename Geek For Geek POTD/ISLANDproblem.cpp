974cce3c47fb790d95da269f3c68e3d64d4ef10e
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-10-06 22:32:15 +0530

    Create ISLANDproblem.cpp

---
+++ Geek For Geek POTD/ISLANDproblem.cpp
@@ -0,0 +1,29 @@
+/*
+
+Find the number of islands
+Difficulty: MediumAccuracy: 42.12%Submissions: 209K+Points: 4
+Given a grid of size n*m (n is the number of rows and m is the number of columns in the grid) consisting of '0's (Water) and '1's(Land). Find the number of islands.
+
+Note: An island is either surrounded by water or the boundary of a grid and is formed by connecting adjacent lands horizontally or vertically or diagonally i.e., in all 8 directions.
+
+Examples:
+
+Input: grid = [[0,1],[1,0],[1,1],[1,0]]
+Output: 1
+Explanation:
+The grid is-
+
+All lands are connected.
+Input: grid = [[0,1,1,1,0,0,0],[0,0,1,1,0,1,0]]
+Output: 2
+Expanation:
+The grid is-
+ 
+There are two islands :- one is colored in "blue" and other in "red".
+Expected Time Complexity: O(n*m)
+Expected Space Complexity: O(n*m)
+
+Constraints:
+1 ≤ n, m ≤ 500
+grid[i][j] = {'0', '1'}
+*/
