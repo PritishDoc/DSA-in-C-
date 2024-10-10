fa395e81c9aa1433e9318c230237ff9c6925e9dd
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-10-10 21:18:40 +0530

    Update MaximumWidthRamp.cpp

--- Leetcode_Problem/MaximumWidthRamp.cpp
+++ Leetcode_Problem/MaximumWidthRamp.cpp
@@ -2,7 +2,7 @@
 Maximum Width Ramp
 Medium
 
-Companies
+
 A ramp in an integer array nums is a pair (i, j) for which i < j and nums[i] <= nums[j]. The width of such a ramp is j - i.
 
 Given an integer array nums, return the maximum width of a ramp in nums. If there is no ramp in nums, return 0.
