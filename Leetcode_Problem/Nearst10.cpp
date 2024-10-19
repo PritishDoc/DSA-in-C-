0a7e43e070e940550870571aebf1086a4a8e0c3d
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-10-19 20:44:32 +0530

    Update Nearst10.cpp

--- Leetcode_Problem/Nearst10.cpp
+++ Leetcode_Problem/Nearst10.cpp
@@ -18,3 +18,34 @@ Constraints:
 1 <= str.size()<= 105
 
 */
+class Solution {
+public:
+    string roundToNearest(string str) {
+        int n = str.size();
+        int lastDigit = str[n - 1] - '0';  // Extract the last digit
+
+        // If the last digit is less than or equal to 5, round down
+        if (lastDigit <= 5) {
+            str[n - 1] = '0';  // Set last digit to '0'
+        } else {
+            // If the last digit is greater than 5, round up
+            int i = n - 1;
+            str[i] = '0';  // Set the last digit to '0'
+
+            // Carry over the rounding process from right to left
+            i--;
+            while (i >= 0 && str[i] == '9') {
+                str[i] = '0';  // Set current digit to 0 and carry the 1 to the left
+                i--;
+            }
+            if (i >= 0) {
+                str[i]++;  // Increase the current digit if it's not already at 9
+            } else {
+                // If all digits were '9', prepend '1' to the number
+                str = '1' + str;
+            }
+        }
+
+        return str;
+    }
+};
