9be87cf577a7cce85c1fd75864ebc493289bbc53
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-10-22 20:29:20 +0530

    Update kthlargest.cpp

--- Leetcode_Problem/kthlargest.cpp
+++ Leetcode_Problem/kthlargest.cpp
@@ -41,3 +41,47 @@ The number of nodes in the tree is n.
 1 <= Node.val <= 106
 1 <= k <= n
 */
+class Solution {
+public:
+    long long kthLargestLevelSum(TreeNode* root, int k) {
+        if (!root) return -1;
+
+        // Min-heap to store the top k largest level sums
+        priority_queue<long long, vector<long long>, greater<long long>> minHeap;
+        
+        // Queue for level-order traversal
+        queue<TreeNode*> q;
+        q.push(root);
+
+        // Perform level-order traversal
+        while (!q.empty()) {
+            int size = q.size();
+            long long levelSum = 0;
+
+            // Sum up all node values at the current level
+            for (int i = 0; i < size; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                levelSum += node->val;
+
+                // Enqueue child nodes for the next level
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            // Push the current level sum to the heap
+            minHeap.push(levelSum);
+
+            // If heap size exceeds k, pop the smallest element
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+
+        // If there are fewer than k levels, return -1
+        if (minHeap.size() < k) return -1;
+
+        // The top of the heap is the Kth largest level sum
+        return minHeap.top();
+    }
+};
