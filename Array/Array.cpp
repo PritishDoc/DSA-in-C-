b2f132e0c870e426db6f72ccc2dbea38f0855c06
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-10-24 21:28:31 +0530

    ss

---
+++ Array/Array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+int marks[100] ={33,45,67,89,3,44,5};
+    double price[56]={33.45,44.99,44.89,98,88};
+
+    //running loops
+    int sz=sizeof(marks);
+    for(int i=0;i<=sz;i++){
+        cout<<marks[i]<<endl;;
+
+    }
+    cout<<endl;
+}
