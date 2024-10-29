f8fadf45300e7038c758b9614b9fb5731b3d603f
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-10-29 17:24:54 +0530

    Update QuickSortOnLimitedList.cpp

--- Leetcode_Problem/QuickSortOnLimitedList.cpp
+++ Leetcode_Problem/QuickSortOnLimitedList.cpp
@@ -20,3 +20,60 @@ Constraints:
 1<= size of linked list <= 105
 
 */
+#include <bits/stdc++.h>
+using namespace std;
+
+// Linked List Node
+struct Node {
+    int data;
+    Node* next;
+    Node(int x) : data(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    // Split the linked list into two halves
+    Node* split(Node* head) {
+        Node* slow = head;
+        Node* fast = head->next;
+        
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        Node* mid = slow->next;
+        slow->next = nullptr;
+        return mid;
+    }
+
+    // Merge two sorted linked lists
+    Node* merge(Node* left, Node* right) {
+        if (!left) return right;
+        if (!right) return left;
+
+        if (left->data < right->data) {
+            left->next = merge(left->next, right);
+            return left;
+        } else {
+            right->next = merge(left, right->next);
+            return right;
+        }
+    }
+
+    // Main function to sort the linked list using merge sort
+    Node* mergeSort(Node* head) {
+        if (!head || !head->next) return head;
+
+        Node* mid = split(head);
+        Node* left = mergeSort(head);
+        Node* right = mergeSort(mid);
+
+        return merge(left, right);
+    }
+
+    // Wrapper function for merge sort
+    Node* quickSort(Node* head) {
+        return mergeSort(head); // Calling merge sort instead
+    }
+};
