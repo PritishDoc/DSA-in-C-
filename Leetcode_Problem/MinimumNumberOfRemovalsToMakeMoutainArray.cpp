c9a4993b6911eb44c844b651d6367a76e55429b5
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-10-30 18:45:34 +0530

    Update MinimumNumberOfRemovalsToMakeMoutainArray.cpp

--- Leetcode_Problem/MinimumNumberOfRemovalsToMakeMoutainArray.cpp
+++ Leetcode_Problem/MinimumNumberOfRemovalsToMakeMoutainArray.cpp
@@ -34,3 +34,40 @@ Constraints:
 It is guaranteed that you can make a mountain array out of nums
 
 */
+class Solution {
+public:
+    int minimumMountainRemovals(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> LIS(n, 1); // Longest Increasing Subsequence up to index i
+        vector<int> LDS(n, 1); // Longest Decreasing Subsequence starting from index i
+
+        // Compute LIS for each element
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    LIS[i] = max(LIS[i], LIS[j] + 1);
+                }
+            }
+        }
+
+        // Compute LDS for each element
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = n - 1; j > i; --j) {
+                if (nums[i] > nums[j]) {
+                    LDS[i] = max(LDS[i], LDS[j] + 1);
+                }
+            }
+        }
+
+        int maxMountainLength = 0;
+        // Find the maximum mountain array length
+        for (int i = 1; i < n - 1; ++i) {
+            if (LIS[i] > 1 && LDS[i] > 1) { // Valid peak
+                maxMountainLength = max(maxMountainLength, LIS[i] + LDS[i] - 1);
+            }
+        }
+
+        // Minimum removals = total elements - max mountain length
+        return n - maxMountainLength;
+    }
+};
