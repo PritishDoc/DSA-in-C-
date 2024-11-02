59df5041eb3f8eda928fc01d0aee2dc68134728e
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-11-02 14:36:35 +0530

    Update CircularDistance.cpp

--- Leetcode_Problem/CircularDistance.cpp
+++ Leetcode_Problem/CircularDistance.cpp
@@ -17,8 +17,6 @@ For example, "leetcode exercises sound delightful", "eetcode", "leetcode eats so
 
 Given a string sentence, return true if it is circular. Otherwise, return false.
 
-
-
 Example 1:
 
 Input: sentence = "leetcode exercises sound delightful"
