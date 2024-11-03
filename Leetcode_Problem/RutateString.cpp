866bfab4e1c9e02acd799e43fa248a01d70e104e
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-11-03 21:26:22 +0530

    Update RutateString.cpp

--- Leetcode_Problem/RutateString.cpp
+++ Leetcode_Problem/RutateString.cpp
@@ -3,7 +3,7 @@
 Rotate String
 Easy
 Topics
-Companies
+
 Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
 
 A shift on s consists of moving the leftmost character of s to the rightmost position.
