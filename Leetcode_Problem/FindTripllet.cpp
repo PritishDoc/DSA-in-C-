4abac73ff66b66fc377ca4be8e87054448b03d4c
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-11-04 20:42:02 +0530

    Update FindTripllet.cpp

--- Leetcode_Problem/FindTripllet.cpp
+++ Leetcode_Problem/FindTripllet.cpp
@@ -26,3 +26,24 @@ Constraints:
 
 
 */
+class Solution {
+  public:
+    vector<vector<int>> findTriplets(vector<int> &arr) {
+        // Code here
+        vector<vector<int>> ans;
+        int n = arr.size();
+        for(int i=0; i<n; i++){
+            int a = arr[i];
+            for(int j=i+1; j<n; j++){
+                int b = arr[j];
+                for(int k=j+1; k<n; k++){
+                    int c  = arr[k];
+                    if(a+b+c == 0){
+                        ans.push_back({i, j, k});
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
