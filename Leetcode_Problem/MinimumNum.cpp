7ae73375209dfafc680f5e480e81333ab816b8b4
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-11-05 21:50:35 +0530

    Update MinimumNum.cpp

--- Leetcode_Problem/MinimumNum.cpp
+++ Leetcode_Problem/MinimumNum.cpp
@@ -41,3 +41,15 @@ Constraints:
 s has an even length.
 s[i] is either '0' or '1
 */
+class Solution {
+public:
+    int minChanges(const std::string& s) {
+    int count = 0;
+    for (int i = 0; i < s.size() - 1; i += 2) {
+        if (s[i] != s[i + 1]) {
+            count++;
+        }
+    }
+    return count;
+}
+};
