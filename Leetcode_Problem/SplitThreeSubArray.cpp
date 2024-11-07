9b51f9d072717b34a153f11d3340abfe52714829
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-11-07 21:44:19 +0530

    Create SplitThreeSubArray.cpp

---
+++ Leetcode_Problem/SplitThreeSubArray.cpp
@@ -0,0 +1,23 @@
+/*
+
+Split array in three equal sum subarrays
+Difficulty: MediumAccuracy: 52.9%Submissions: 16K+Points: 4
+Given an array, arr[], determine if arr can be split into three consecutive parts such that the sum of each part is equal. If possible, return any index pair(i, j) in an array such that sum(arr[0..i]) = sum(arr[i+1..j]) = sum(arr[j+1..n-1]), otherwise return an array {-1,-1}.
+
+Note: Since multiple answers are possible, return any of them. The driver code will print true if it is correct otherwise, it will print false.
+
+Examples :
+
+Input:  arr[] = [1, 3, 4, 0, 4]
+Output: true
+Explanation: [1, 2] is valid pair as sum of subarray arr[0..1] is equal to sum of subarray arr[2..3] and also to sum of subarray arr[4..4]. The sum is 4, so driver code prints true.
+Input: arr[] = [2, 3, 4]
+Output: false
+Explanation: No three subarrays exist which have equal sum.
+Input: arr[] = [0, 1, 1]
+Output: false
+Constraints:
+3 ≤ arr.size() ≤ 106
+0 ≤ arr[i] ≤ 106
+
+*/
