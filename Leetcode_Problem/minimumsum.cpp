8a344ca331a70989f13c573fab7d861f1845e38e
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-11-09 22:16:38 +0530

    Update minimumsum.cpp

--- Leetcode_Problem/minimumsum.cpp
+++ Leetcode_Problem/minimumsum.cpp
@@ -19,3 +19,24 @@ Constraints:
 0 ≤ arr[i] ≤ 9
 
 */
+class Solution {
+  public:
+    string minSum(vector<int> &arr) {
+        // code here
+        sort( arr.begin(), arr.end(), greater<int>() );
+        int n = arr.size(), sum=0;
+        vector<char> v;
+        for( int i=0; i<n; i+=2 ) {
+            sum += arr[i];
+            if( (i+1) < n )
+                sum += arr[i+1];
+            v.push_back( '0' + sum%10 );
+            sum = sum/10;
+        }
+        if( sum )
+            v.push_back( '0' + sum );
+        while( v.back() == '0' )
+            v.pop_back();
+        return string( v.rbegin(), v.rend() );
+    }
+};
