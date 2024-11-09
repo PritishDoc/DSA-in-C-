53658585d50833d15deb55f85ec93040c61b4a4a
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-11-09 22:21:31 +0530

    Update minimumarraySum.cpp

--- Leetcode_Problem/minimumarraySum.cpp
+++ Leetcode_Problem/minimumarraySum.cpp
@@ -1,7 +1,7 @@
 /*
 
  Minimum Array End
-Medium
+
 Hint
 You are given two integers n and x. You have to construct an array of positive integers nums of size n where for every 0 <= i < n - 1, nums[i + 1] is greater than nums[i], and the result of the bitwise AND operation between all elements of nums is x.
 
