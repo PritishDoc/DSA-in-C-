b63a17f47100e75e6a05d4161b7b41c9dce2e4cd
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-11-09 22:20:56 +0530

    Update minimumarraySum.cpp

--- Leetcode_Problem/minimumarraySum.cpp
+++ Leetcode_Problem/minimumarraySum.cpp
@@ -38,3 +38,21 @@ Constraints:
 1 <= n, x <= 108
 
 */
+class Solution {
+public:
+    long minEnd(int n, int x) {
+        long result = x;
+        long remaining = n - 1;
+        long position = 1;
+        
+        while (remaining) {
+            if (!(x & position)) {
+                result |= (remaining & 1) * position;
+                remaining >>= 1;
+            }
+            position <<= 1;
+        }
+        
+        return result;
+    }
+};
