1cd8495d8aaa732dfaad34c6161dea6b0b10ff1e
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-11-17 21:18:53 +0530

    Update ShortestSubarray.cpp

--- Leetcode_Problem/ShortestSubarray.cpp
+++ Leetcode_Problem/ShortestSubarray.cpp
@@ -1,8 +1,7 @@
 /*
 Shortest Subarray with Sum at Least K
 Hard
-Topics
-Companies
+
 Given an integer array nums and an integer k, return the length of the shortest non-empty subarray of nums with a sum of at least k. If there is no such subarray, return -1.
 
 A subarray is a contiguous part of an array.
