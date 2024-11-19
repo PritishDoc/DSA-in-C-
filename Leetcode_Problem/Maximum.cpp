7e5167ba7512bcff0c22ce9bbae1233ee512d5ec
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-11-19 20:16:30 +0530

    Update Maximum.cpp

--- Leetcode_Problem/Maximum.cpp
+++ Leetcode_Problem/Maximum.cpp
@@ -1,9 +1,7 @@
 /*
  Maximum Sum of Distinct Subarrays With Length K
 Medium
-Topics
-Companies
-Hint
+
 You are given an integer array nums and an integer k. Find the maximum subarray sum of all the subarrays of nums that meet the following conditions:
 
 The length of the subarray is k, and
