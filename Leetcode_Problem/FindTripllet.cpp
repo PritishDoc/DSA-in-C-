bede6660e171cb2e23cc0b260c117a6427a344b3
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-11-21 21:40:27 +0530

    Update FindTripllet.cpp

--- Leetcode_Problem/FindTripllet.cpp
+++ Leetcode_Problem/FindTripllet.cpp
@@ -4,6 +4,7 @@ Find All Triplets with Zero Sum
 Difficulty: EasyAccuracy: 50.36%Submissions: 16K+Points: 2
 Given an array arr[], find all possible indices [i, j, k] of triplets [arr[i], arr[j], arr[k]] in the array whose sum is equal to zero. Return indices of triplets in any order and all the returned triplets indices should also be internally sorted, i.e., for any triplet indices [i, j, k], the condition i < j < k should hold.
 
+
 Note: Try to solve this using the O(n2) approach.
 
 Examples:
