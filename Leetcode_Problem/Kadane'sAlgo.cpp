905431296a8182fab0f8d40844d00368a98bdd2f
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-11-21 11:44:57 +0530

    k

---
+++ Leetcode_Problem/Kadane'sAlgo.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5]={1,3,-4,8,-3};
+
+    int currsum=0;
+    int maxsum=INT8_MIN;
+    int n=5;
+    for(int i=0;i<n;i++){
+        currsum+=arr[i];
+        maxsum=max(currsum,maxsum);
+        if(currsum<0){
+            currsum=0;
+        }
+    }
+    cout<<maxsum;
+}
