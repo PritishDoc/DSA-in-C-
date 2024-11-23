bad07c1005a0c3ee45d5f32a99e83e704824abea
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-11-23 20:28:49 +0530

    Update RotatingThestring.cpp

--- Leetcode_Problem/RotatingThestring.cpp
+++ Leetcode_Problem/RotatingThestring.cpp
@@ -58,3 +58,32 @@ n == box[i].length
 box[i][j] is either '#', '*', or '.'.
 
 */
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        int m = box.size(), n = box[0].size();
+        
+        // Step 1: Simulate gravity for each row
+        for (int i = 0; i < m; i++) {
+            int fallPos = n - 1; // Rightmost position
+            for (int j = n - 1; j >= 0; j--) {
+                if (box[i][j] == '*') {
+                    fallPos = j - 1; // Reset fall position to just left of the obstacle
+                } else if (box[i][j] == '#') {
+                    swap(box[i][j], box[i][fallPos]); // Move stone to the fall position
+                    fallPos--; // Update fall position
+                }
+            }
+        }
+        
+        // Step 2: Rotate the box 90 degrees clockwise
+        vector<vector<char>> rotatedBox(n, vector<char>(m));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rotatedBox[j][m - 1 - i] = box[i][j];
+            }
+        }
+        
+        return rotatedBox;
+    }
+};
