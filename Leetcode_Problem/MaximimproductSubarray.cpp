961c957b18f032c6db04b8f6551f50ffae1c2fdc
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-11-25 21:05:33 +0530

    Create MaximimproductSubarray.cpp

---
+++ Leetcode_Problem/MaximimproductSubarray.cpp
@@ -0,0 +1,22 @@
+/*
+Difficulty: MediumAccuracy: 18.09%Submissions: 398K+Points: 4
+Given an array arr[] that contains positive and negative integers (may contain 0 as well). Find the maximum product that we can get in a subarray of arr.
+
+Note: It is guaranteed that the output fits in a 32-bit integer.
+
+Examples
+
+Input: arr[] = [-2, 6, -3, -10, 0, 2]
+Output: 180
+Explanation: The subarray with maximum product is {6, -3, -10} with product = 6 * (-3) * (-10) = 180.
+Input: arr[] = [-1, -3, -10, 0, 6]
+Output: 30
+Explanation: The subarray with maximum product is {-3, -10} with product = (-3) * (-10) = 30.
+Input: arr[] = [2, 3, 4] 
+Output: 24 
+Explanation: For an array with all positive elements, the result is product of all elements. 
+Constraints:
+1 ≤ arr.size() ≤ 106
+-10  ≤  arr[i]  ≤  10
+
+*/
