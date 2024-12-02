7f9a533794bf106e9e0ead56541083cf544fc3ee
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-12-02 20:39:27 +0530

    Update checkword.cpp

--- Leetcode_Problem/checkword.cpp
+++ Leetcode_Problem/checkword.cpp
@@ -1,7 +1,7 @@
 /*
 Check If a Word Occurs As a Prefix of Any Word in a Sentence
 Easy
-Topics
+
 
 Hint
 Given a sentence that consists of some words separated by a single space, and a searchWord, check if searchWord is a prefix of any word in sentence.
