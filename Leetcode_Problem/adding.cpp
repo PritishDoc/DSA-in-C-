4f6c24c4e085ed5a7446e486b6be89b2931926f1
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-12-03 20:54:08 +0530

    Update adding.cpp

--- Leetcode_Problem/adding.cpp
+++ Leetcode_Problem/adding.cpp
@@ -42,3 +42,22 @@ s consists only of lowercase and uppercase English letters.
 All the values of spaces are strictly increasing.
 
 */
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        string result;
+        int spaceIndex = 0; // Pointer for the spaces array
+        int n = spaces.size();
+        
+        for (int i = 0; i < s.size(); ++i) {
+            // If the current index matches the next space index, add a space
+            if (spaceIndex < n && i == spaces[spaceIndex]) {
+                result += ' ';
+                ++spaceIndex;
+            }
+            // Add the current character
+            result += s[i];
+        }
+        return result;
+    }
+};
