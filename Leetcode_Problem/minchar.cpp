842fd623d1c9d15abd4500c84660d1419b55a497
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-12-03 21:02:58 +0530

    Update minchar.cpp

--- Leetcode_Problem/minchar.cpp
+++ Leetcode_Problem/minchar.cpp
@@ -17,3 +17,34 @@ Constraints:
 1 <= s.size() <= 106
 
 */
+class Solution {
+public:
+    int minChar(string& s) {
+        string rev_s = s; // Reverse the string
+        reverse(rev_s.begin(), rev_s.end());
+
+        // Create a new string with separator to avoid overlap
+        string combined = s + "#" + rev_s;
+
+        // Apply KMP algorithm to find the longest prefix-suffix match
+        vector<int> lps(combined.size(), 0);
+        
+        // Build the prefix function for the combined string
+        for (int i = 1; i < combined.size(); ++i) {
+            int j = lps[i - 1];
+            while (j > 0 && combined[i] != combined[j]) {
+                j = lps[j - 1];
+            }
+            if (combined[i] == combined[j]) {
+                j++;
+            }
+            lps[i] = j;
+        }
+
+        // The last value in lps array gives the length of the longest palindromic suffix
+        int longest_palindromic_suffix = lps[combined.size() - 1];
+
+        // To make the string a palindrome, we need to add characters before the string
+        return s.size() - longest_palindromic_suffix;
+    }
+};
