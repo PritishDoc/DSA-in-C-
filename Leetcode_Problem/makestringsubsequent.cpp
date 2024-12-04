ecb127c46a3a9e1bf63a233718d0fe7975bbbfef
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-12-04 22:42:51 +0530

    Update makestringsubsequent.cpp

--- Leetcode_Problem/makestringsubsequent.cpp
+++ Leetcode_Problem/makestringsubsequent.cpp
@@ -45,3 +45,22 @@ Constraints:
 str1 and str2 consist of only lowercase English letters.
 
 */
+class Solution {
+public:
+    bool canMakeSubsequence(string str1, string str2) {
+        int n = str1.size(), m = str2.size();
+        int i = 0, j = 0;
+
+        // Traverse both strings
+        while (i < n && j < m) {
+            // Check if the current character in str1 matches the character in str2
+            if (str1[i] == str2[j] || ((str1[i] - 'a' + 1) % 26 + 'a') == str2[j]) {
+                j++; // Move to the next character in str2
+            }
+            i++; // Move to the next character in str1
+        }
+
+        // If all characters of str2 are matched, return true
+        return j == m;
+    }
+};
