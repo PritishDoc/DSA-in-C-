0439bbb2d9f624ff1184d0c5122aa3ba10a0105d
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-12-04 22:36:56 +0530

    Create stringrotationeachother.cpp

---
+++ Leetcode_Problem/stringrotationeachother.cpp
@@ -0,0 +1,22 @@
+/*
+
+trings Rotations of Each Other
+Difficulty: EasyAccuracy: 43.83%Submissions: 216K+Points: 2
+You are given two strings of equal lengths, s1 and s2. The task is to check if s2 is a rotated version of the string s1.
+
+Note: The characters in the strings are in lowercase.
+
+Examples :
+
+Input: s1 = "abcd", s2 = "cdab"
+Output: true
+Explanation: After 2 right rotations, s1 will become equal to s2.
+Input: s1 = "aab", s2 = "aba"
+Output: true
+Explanation: After 1 left rotation, s1 will become equal to s2.
+Input: s1 = "abcd", s2 = "acbd"
+Output: false
+Explanation: Strings are not rotations of each other.
+Constraints:
+1 <= s1.size(), s2.size() <= 105
+*/
