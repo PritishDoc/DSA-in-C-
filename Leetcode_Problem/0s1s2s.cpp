c13de08714cf37c5fe973354cf3511b095bcb985
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-12-05 19:59:44 +0530

    Create 0s1s2s.cpp

---
+++ Leetcode_Problem/0s1s2s.cpp
@@ -0,0 +1,20 @@
+/*
+Sort 0s, 1s and 2s
+Difficulty: EasyAccuracy: 50.58%Submissions: 722K+Points: 2
+Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
+
+Examples:
+
+Input: arr[] = [0, 1, 2, 0, 1, 2]
+Output: [0, 0, 1, 1, 2, 2]
+Explanation: 0s 1s and 2s are segregated into ascending order.
+Input: arr[] = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
+Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
+Explanation: 0s 1s and 2s are segregated into ascending order.
+Constraints:
+1 <= arr.size() <= 106
+0 <= arr[i] <= 2
+
+
+
+*/
