3fc98db81b9b49c7ce7a02cb719d3fb06127e98c
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-12-05 19:58:13 +0530

    Update MovepicesToObtainString.cpp

--- Leetcode_Problem/MovepicesToObtainString.cpp
+++ Leetcode_Problem/MovepicesToObtainString.cpp
@@ -1,6 +1,5 @@
 /*
 Move Pieces to Obtain a String
-Medium
 Hint
 You are given two strings start and target, both of length n. Each string consists only of the characters 'L', 'R', and '_' where:
 
