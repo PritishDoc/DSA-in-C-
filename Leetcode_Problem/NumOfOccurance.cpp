4e62351c405838980dc01e028136b961bd3cd95d
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-12-12 19:50:01 +0530

    Create NumOfOccurance.cpp

---
+++ Leetcode_Problem/NumOfOccurance.cpp
@@ -0,0 +1,23 @@
+/*
+Difficulty: EasyAccuracy: 59.34%Submissions: 263K+Points: 2
+Given a sorted array, arr[] and a number target, you need to find the number of occurrences of target in arr[]. 
+
+Examples :
+
+Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 2
+Output: 4
+Explanation: target = 2 occurs 4 times in the given array so the output is 4.
+Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 4
+Output: 0
+Explanation: target = 4 is not present in the given array so the output is 0.
+Input: arr[] = [8, 9, 10, 12, 12, 12], target = 12
+Output: 3
+Explanation: target = 12 occurs 3 times in the given array so the output is 3.
+Constraints:
+1 ≤ arr.size() ≤ 106
+1 ≤ arr[i] ≤ 106
+1 ≤ target ≤ 106
+
+
+
+*/
