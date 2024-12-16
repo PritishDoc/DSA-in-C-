623270e21e3af6afc8149c726f89a8b4813c641e
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-12-16 09:59:03 +0530

    Update x2.cpp

--- Leetcode_Problem/x2.cpp
+++ Leetcode_Problem/x2.cpp
@@ -50,3 +50,23 @@ Constraints:
 1 <= multiplier <= 5
 
 */
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<int> getFinalState(std::vector<int>& nums, int k, int multiplier) {
+        for (int i = 0; i < k; ++i) {
+            // Find the index of the first minimum element
+            int minIndex = 0;
+            for (int j = 1; j < nums.size(); ++j) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Multiply the minimum value by the multiplier
+            nums[minIndex] *= multiplier;
+        }
+        return nums;
+    }
+};
