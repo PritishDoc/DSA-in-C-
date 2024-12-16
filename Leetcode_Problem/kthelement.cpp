cb43f782178853a471fde6b4729ba5dc7ec436eb
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-12-16 10:02:20 +0530

    Update kthelement.cpp

--- Leetcode_Problem/kthelement.cpp
+++ Leetcode_Problem/kthelement.cpp
@@ -19,3 +19,24 @@ Constraints:
 0 <= a[i], b[i] < 108
 
 */
+
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<int> getFinalState(std::vector<int>& nums, int k, int multiplier) {
+        for (int i = 0; i < k; ++i) {
+            // Find the index of the first minimum element
+            int minIndex = 0;
+            for (int j = 1; j < nums.size(); ++j) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Multiply the minimum value by the multiplier
+            nums[minIndex] *= multiplier;
+        }
+        return nums;
+    }
+};
