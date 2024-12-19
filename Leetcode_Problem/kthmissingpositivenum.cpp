c4a42c4cffabcab396364cd8d92a561dd982bab9
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-12-19 22:04:16 +0530

    Create kthmissingpositivenum.cpp

---
+++ Leetcode_Problem/kthmissingpositivenum.cpp
@@ -0,0 +1,24 @@
+/*
+Kth Missing Positive Number in a Sorted Array
+Difficulty: MediumAccuracy: 53.02%Submissions: 18K+Points: 4
+Given a sorted array of distinct positive integers arr[], we need to find the kth positive number that is missing from arr[].  
+
+Examples :
+
+Input: arr[] = [2, 3, 4, 7, 11], k = 5
+Output: 9
+Explanation: Missing are 1, 5, 6, 8, 9, 10… and 5th missing number is 9.
+Input: arr[] = [1, 2, 3], k = 2
+Output: 5
+Explanation: Missing are 4, 5, 6… and 2nd missing number is 5.
+Input: arr[] = [3, 5, 9, 10, 11, 12], k = 2
+Output: 2
+Explanation: Missing are 1, 2, 4, 6… and 2nd missing number is 2.
+Constraints:
+1 <= arr.size() <= 105
+1 <= k <= 105
+1 <= arr[i]<= 106
+
+
+
+*/
