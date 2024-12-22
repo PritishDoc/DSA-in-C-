51db47fe6767ddd5cc24d06e612c43db30704a67
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-12-22 20:11:20 +0530

    Update SearchInarow.cpp

--- Leetcode_Problem/SearchInarow.cpp
+++ Leetcode_Problem/SearchInarow.cpp
@@ -23,3 +23,25 @@ Constraints:
 
 
 */
+class Solution {
+public:
+    bool matSearch(vector<vector<int>> &mat, int x) {
+        int n = mat.size();       // Number of rows
+        int m = mat[0].size();    // Number of columns
+
+        // Start from the top-right corner
+        int row = 0, col = m - 1;
+
+        while (row < n && col >= 0) {
+            if (mat[row][col] == x) {
+                return true; // Element found
+            } else if (mat[row][col] > x) {
+                col--; // Move left
+            } else {
+                row++; // Move down
+            }
+        }
+
+        return false; // Element not found
+    }
+};
