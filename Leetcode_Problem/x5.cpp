10b8fabe4efd8a070e5c4ec34028f8495d724d59
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-12-27 22:43:49 +0530

    Update x5.cpp

--- Leetcode_Problem/x5.cpp
+++ Leetcode_Problem/x5.cpp
@@ -25,3 +25,19 @@ Constraints:
 2 <= values.length <= 5 * 104
 1 <= values[i] <= 1000
 */
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& values) {
+        int maxScore = 0;
+        int max_i = values[0]; // Maximum value of values[i] + i seen so far
+
+        for (int j = 1; j < values.size(); ++j) {
+            // Calculate the score for the current pair
+            maxScore = max(maxScore, max_i + values[j] - j);
+            // Update max_i for the next iteration
+            max_i = max(max_i, values[j] + j);
+        }
+
+        return maxScore;
+    }
+};
