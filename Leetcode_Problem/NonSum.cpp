eebd4a07c0a677bbe6b647d341d171e5aaffaa82
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2024-12-28 20:05:03 +0530

    Update NonSum.cpp

--- Leetcode_Problem/NonSum.cpp
+++ Leetcode_Problem/NonSum.cpp
@@ -3,7 +3,6 @@
 
 
 Maximum Sum of 3 Non-Overlapping Subarrays
-Hard
 Given an integer array nums and an integer k, find three non-overlapping subarrays of length k with maximum sum and return them.
 
 Return the result as a list of indices representing the starting position of each interval (0-indexed). If there are multiple answers, return the lexicographically smallest one.
