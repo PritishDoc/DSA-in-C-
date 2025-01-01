1122fe600843c04339d9c6620c98e3470c99dcbe
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-01 21:11:58 +0530

    Update maximumarray.cpp

--- Leetcode_Problem/maximumarray.cpp
+++ Leetcode_Problem/maximumarray.cpp
@@ -1,7 +1,6 @@
 /*
 
  Maximum Score After Splitting a String
-Easy
 Hint
 Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings (i.e. left substring and right substring).
 
