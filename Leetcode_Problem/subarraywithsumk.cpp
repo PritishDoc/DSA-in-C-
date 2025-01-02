b24884aa11fb76888d26e596e546dbd2c738b43e
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-02 22:18:16 +0530

    Create subarraywithsumk.cpp

---
+++ Leetcode_Problem/subarraywithsumk.cpp
@@ -0,0 +1,24 @@
+/*
+
+Subarrays with sum K
+Difficulty: MediumAccuracy: 49.74%Submissions: 57K+Points: 4
+Given an unsorted array of integers, find the number of continuous subarrays having sum exactly equal to a given number k.
+
+Examples:
+
+Input: arr = [10, 2, -2, -20, 10], k = -10
+Output: 3
+Explaination: Subarrays: arr[0...3], arr[1...4], arr[3...4] have sum exactly equal to -10.
+Input: arr = [9, 4, 20, 3, 10, 5], k = 33
+Output: 2
+Explaination: Subarrays: arr[0...2], arr[2...4] have sum exactly equal to 33.
+Input: arr = [1, 3, 5], k = 0
+Output: 0
+Explaination: No subarray with 0 sum.
+Constraints:
+
+1 ≤ arr.size() ≤ 105
+-103 ≤ arr[i] ≤ 103
+-107 ≤ k ≤ 10
+
+*/
