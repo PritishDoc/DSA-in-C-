e6efaedeb12b4d88a47bcaff08be4ff3665411c0
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-05 21:25:37 +0530

    Update shiftingletter2.cpp

--- Leetcode_Problem/shiftingletter2.cpp
+++ Leetcode_Problem/shiftingletter2.cpp
@@ -1,7 +1,6 @@
 /*
 
 Shifting Letters II
-Medium
 Hint
 You are given a string s of lowercase English letters and a 2D integer array shifts where shifts[i] = [starti, endi, directioni]. For every i, shift the characters in s from the index starti to the index endi (inclusive) forward if directioni = 1, or shift the characters backward if directioni = 0.
 
