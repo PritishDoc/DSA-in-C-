03d6d6347b7b6b05bb6910020df803c363223fee
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-06 22:11:03 +0530

    Update minimumnumbox.cpp

--- Leetcode_Problem/minimumnumbox.cpp
+++ Leetcode_Problem/minimumnumbox.cpp
@@ -2,7 +2,6 @@
  Minimum Number of Operations to Move All Balls to Each Box
 Medium
 Topics
-Companies
 Hint
 You have n boxes. You are given a binary string boxes of length n, where boxes[i] is '0' if the ith box is empty, and '1' if it contains one ball.
 
