88e00c327c2ec77b26e29e059e8cb90c1f79d76a
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-06 22:10:34 +0530

    Update minimumnumbox.cpp

--- Leetcode_Problem/minimumnumbox.cpp
+++ Leetcode_Problem/minimumnumbox.cpp
@@ -34,3 +34,33 @@ n == boxes.length
 1 <= n <= 2000
 boxes[i] is either '0' or '1'.
 */
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        int n = boxes.size();
+        vector<int> answer(n, 0);
+        
+        // Left-to-right pass
+        int balls = 0, operations = 0;
+        for (int i = 0; i < n; ++i) {
+            answer[i] += operations;
+            balls += (boxes[i] == '1');
+            operations += balls;
+        }
+        
+        // Right-to-left pass
+        balls = 0;
+        operations = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            answer[i] += operations;
+            balls += (boxes[i] == '1');
+            operations += balls;
+        }
+        
+        return answer;
+    }
+};
