77bcf9f94038d7bb91e40572d1af14242e83b784
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-07 22:11:07 +0530

    Update stringmachingarray.cpp

--- Leetcode_Problem/stringmachingarray.cpp
+++ Leetcode_Problem/stringmachingarray.cpp
@@ -1,7 +1,6 @@
 /*
 
  String Matching in an Array
-Easy
 Hint
 Given an array of string words, return all strings in words that is a substring of another word. You can return the answer in any order.
 
