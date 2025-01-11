a11b69a46b8493d7970f4d4d05a636bb537ea646
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-11 21:36:54 +0530

    Create longestsubstring.cpp

---
+++ Leetcode_Problem/longestsubstring.cpp
@@ -0,0 +1,21 @@
+/*
+Longest substring with distinct characters
+Difficulty: EasyAccuracy: 31.32%Submissions: 148K+Points: 2
+Given a string s, find the length of the longest substring with all distinct characters. 
+
+Examples:
+
+Input: s = "geeksforgeeks"
+Output: 7
+Explanation: "eksforg" is the longest substring with all distinct characters.
+Input: s = "aaa"
+Output: 1
+Explanation: "a" is the longest substring with all distinct characters.
+Input: s = "abcdefabcbb"
+Output: 6
+Explanation: The longest substring with all distinct characters is "abcdef", which has a length of 6.
+Constraints:
+1<= s.size()<=3*104
+All the characters are in lowercase
+
+*/
