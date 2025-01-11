ed55942d80fdbb2ef1da17d32d285b4428a4117b
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-11 21:34:53 +0530

    Update ConstructKpelindrom.cpp

--- Leetcode_Problem/ConstructKpelindrom.cpp
+++ Leetcode_Problem/ConstructKpelindrom.cpp
@@ -1,6 +1,5 @@
 /*
 Construct K Palindrome Strings
-Medium
 Hint
 Given a string s and an integer k, return true if you can use all the characters in s to construct k palindrome strings or false otherwise.
 
