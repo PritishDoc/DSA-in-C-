6197e9e52b23bcac975f7ef032003f57322786c7
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-12 21:29:28 +0530

    Update trappingrainwater.cpp

--- Leetcode_Problem/trappingrainwater.cpp
+++ Leetcode_Problem/trappingrainwater.cpp
@@ -24,3 +24,34 @@ Constraints:
 0 < arr[i] < 103
 
 */
+class Solution {
+  public:
+    int maxWater(vector<int> &arr) {
+        int n = arr.size();
+        if (n < 3) return 0; // Not enough blocks to trap water
+
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int totalWater = 0;
+
+        while (left <= right) {
+            if (arr[left] < arr[right]) {
+                if (arr[left] >= leftMax) {
+                    leftMax = arr[left];
+                } else {
+                    totalWater += leftMax - arr[left];
+                }
+                left++;
+            } else {
+                if (arr[right] >= rightMax) {
+                    rightMax = arr[right];
+                } else {
+                    totalWater += rightMax - arr[right];
+                }
+                right--;
+            }
+        }
+
+        return totalWater;
+    }
+};
