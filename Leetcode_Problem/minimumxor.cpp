a54537fce6fc67fdd401e36244273b1599e24995
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-15 22:14:20 +0530

    Update minimumxor.cpp

--- Leetcode_Problem/minimumxor.cpp
+++ Leetcode_Problem/minimumxor.cpp
@@ -38,3 +38,31 @@ Constraints:
 1 <= num1, num2 <= 109
 
  */
+#include <bitset>
+
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        int setBitsNum2 = __builtin_popcount(num2); // Count set bits in num2
+        int setBitsX = 0; // Number of set bits in x
+        int x = 0; // Resultant number
+
+        // Set bits in x based on num1
+        for (int i = 31; i >= 0 && setBitsX < setBitsNum2; --i) {
+            if (num1 & (1 << i)) { // Check if bit i in num1 is set
+                x |= (1 << i);    // Set the same bit in x
+                setBitsX++;
+            }
+        }
+
+        // If more set bits are needed, set them from the least significant bit
+        for (int i = 0; i < 32 && setBitsX < setBitsNum2; ++i) {
+            if (!(x & (1 << i))) { // If bit i in x is not set
+                x |= (1 << i);    // Set bit i in x
+                setBitsX++;
+            }
+        }
+
+        return x;
+    }
+};
