3e84892d7795238ade89299a40c95524709c0e2e
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-16 19:36:07 +0530

    Update bitwisexor.cpp

--- Leetcode_Problem/bitwisexor.cpp
+++ Leetcode_Problem/bitwisexor.cpp
@@ -3,7 +3,6 @@
 Bitwise XOR of All Pairings
 Medium
 Topics
-Companies
 Hint
 You are given two 0-indexed arrays, nums1 and nums2, consisting of non-negative integers. There exists another array, nums3, which contains the bitwise XOR of all pairings of integers between nums1 and nums2 (every integer in nums1 is paired with every integer in nums2 exactly once).
 
