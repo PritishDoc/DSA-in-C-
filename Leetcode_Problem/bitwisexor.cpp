dfb50d63ae1845d8ac4602dd4e153362f4e88d4d
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-16 19:38:47 +0530

    jj

--- Leetcode_Problem/bitwisexor.cpp
+++ Leetcode_Problem/bitwisexor.cpp
@@ -32,3 +32,30 @@ Constraints:
 0 <= nums1[i], nums2[j] <= 109
 
 */
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int xor1 = 0, xor2 = 0;
+        
+        // Compute XOR of all elements in nums1
+        for (int num : nums1) {
+            xor1 ^= num;
+        }
+        
+        // Compute XOR of all elements in nums2
+        for (int num : nums2) {
+            xor2 ^= num;
+        }
+        
+        // Result depends on the parity of lengths
+        int result = 0;
+        if (nums2.size() % 2 == 1) {
+            result ^= xor1;
+        }
+        if (nums1.size() % 2 == 1) {
+            result ^= xor2;
+        }
+        
+        return result;
+    }
+};
