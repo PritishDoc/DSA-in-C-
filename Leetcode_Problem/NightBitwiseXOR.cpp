1fd02bf028ae381832431e8b64340ab71dac8e99
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-17 21:06:17 +0530

    Update NightBitwiseXOR.cpp

--- Leetcode_Problem/NightBitwiseXOR.cpp
+++ Leetcode_Problem/NightBitwiseXOR.cpp
@@ -1,6 +1,5 @@
 /*
 Neighboring Bitwise XOR
-Medium
 Hint
 A 0-indexed array derived with length n is derived by computing the bitwise XOR (⊕) of adjacent values in a binary array original of length n.
 
