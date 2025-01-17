d3847e47257de8fb566095c6fe849be86b5fa33a
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-17 21:11:34 +0530

    Update ProductArrayPuzzle.cpp

--- Leetcode_Problem/ProductArrayPuzzle.cpp
+++ Leetcode_Problem/ProductArrayPuzzle.cpp
@@ -24,3 +24,29 @@ Constraints:
 -100 <= arr[i] <= 10
 
 */
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> res(n, 1);
+        
+        // Compute prefix products
+        int prefix = 1;
+        for (int i = 0; i < n; ++i) {
+            res[i] = prefix;
+            prefix *= arr[i];
+        }
+        
+        // Compute suffix products and combine with prefix products
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            res[i] *= suffix;
+            suffix *= arr[i];
+        }
+        
+        return res;
+    }
+};
