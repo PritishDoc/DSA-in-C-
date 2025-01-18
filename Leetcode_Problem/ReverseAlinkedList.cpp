4e701a8ca96ad05c853e1fad8a8d4450329870ec
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-18 20:54:56 +0530

    Create ReverseAlinkedList.cpp

---
+++ Leetcode_Problem/ReverseAlinkedList.cpp
@@ -0,0 +1,24 @@
+/*
+Reverse a linked list
+Difficulty: EasyAccuracy: 73.11%Submissions: 327K+Points: 2
+Given the head of a linked list, the task is to reverse this list and return the reversed head.
+
+Examples:
+
+Input: head: 1 -> 2 -> 3 -> 4 -> NULL
+Output: head: 4 -> 3 -> 2 -> 1 -> NULL
+Explanation:
+
+Input: head: 2 -> 7 -> 10 -> 9 -> 8 -> NULL
+Output: head: 8 -> 9 -> 10 -> 7 -> 2 -> NULL
+Explanation:
+
+Input: head: 2 -> NULL
+Output: 2 -> NULL
+Explanation:
+
+Constraints:
+1 <= number of nodes, data of nodes <= 105
+
+
+*/
