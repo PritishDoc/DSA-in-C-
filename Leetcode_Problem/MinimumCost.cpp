4d21fc9da1bf3bc9f5ce95a23fe86242afae0aea
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-18 20:59:22 +0530

    Update MinimumCost.cpp

--- Leetcode_Problem/MinimumCost.cpp
+++ Leetcode_Problem/MinimumCost.cpp
@@ -2,7 +2,6 @@
 Minimum Cost to Make at Least One Valid Path in a Grid
 Hard
 Topics
-Companies
 Hint
 Given an m x n grid. Each cell of the grid has a sign pointing to the next cell you should visit if you are currently in this cell. The sign of grid[i][j] can be:
 
