c602759b5d0ac3d7519b9a1e5469b8670368c810
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-19 20:35:56 +0530

    Update TrappingRainWater2.cpp

--- Leetcode_Problem/TrappingRainWater2.cpp
+++ Leetcode_Problem/TrappingRainWater2.cpp
@@ -1,7 +1,7 @@
 /*
 
  Trapping Rain Water II
-Hard
+
 Given an m x n integer matrix heightMap representing the height of each unit cell in a 2D elevation map, return the volume of water it can trap after raining.
 
  
