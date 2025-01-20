266a72476de31a351f6ae8ca35a59bdb6e72b490
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-20 20:44:14 +0530

    Create mergetwosortedLinkedlist.cpp

---
+++ Leetcode_Problem/mergetwosortedLinkedlist.cpp
@@ -0,0 +1,21 @@
+/*
+
+Merge two sorted linked lists
+Difficulty: MediumAccuracy: 62.91%Submissions: 172K+Points: 4
+Given the head of two sorted linked lists consisting of nodes respectively. The task is to merge both lists and return the head of the sorted merged list.
+
+Examples:
+
+Input: head1 = 5 -> 10 -> 15 -> 40, head2 = 2 -> 3 -> 20
+Output: 2 -> 3 -> 5 -> 10 -> 15 -> 20 -> 40
+Explanation:
+
+Input: head1 = 1 -> 1, head2 = 2 -> 4
+Output: 1 -> 1 -> 2 -> 4
+Explanation:
+
+Constraints:
+1 <= no. of nodes<= 103
+0 <= node->data <= 10
+
+*/
