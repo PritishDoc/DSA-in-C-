54aaedc659a465fd11e2f82f785add096e5664b7
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-20 21:07:14 +0530

    Update firstcompletelyprintedrow.cpp

--- Leetcode_Problem/firstcompletelyprintedrow.cpp
+++ Leetcode_Problem/firstcompletelyprintedrow.cpp
@@ -1,7 +1,6 @@
 /*
 
 First Completely Painted Row or Column
-Medium
 Hint
 You are given a 0-indexed integer array arr, and an m x n integer matrix mat. arr and mat both contain all the integers in the range [1, m * n].
 
