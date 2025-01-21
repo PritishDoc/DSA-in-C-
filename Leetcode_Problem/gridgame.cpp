ac48f6cf080741745ad076bf229e932f63f5e33b
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-21 21:09:28 +0530

    Update gridgame.cpp

--- Leetcode_Problem/gridgame.cpp
+++ Leetcode_Problem/gridgame.cpp
@@ -1,7 +1,6 @@
 /*
  Grid Game
 Medium
-Topics
 Hint
 You are given a 0-indexed 2D array grid of size 2 x n, where grid[r][c] represents the number of points at position (r, c) on the matrix. Two robots are playing a game on this matrix.
 
