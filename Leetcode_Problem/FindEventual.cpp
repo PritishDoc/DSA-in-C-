def7cef2a85cb40666a3606db7ecd027c21750bb
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-01-24 23:24:11 +0530

    Update FindEventual.cpp

--- Leetcode_Problem/FindEventual.cpp
+++ Leetcode_Problem/FindEventual.cpp
@@ -3,7 +3,6 @@
  Find Eventual Safe States
 Medium
 Topics
-Companies
 There is a directed graph of n nodes with each node labeled from 0 to n - 1. The graph is represented by a 0-indexed 2D integer array graph where graph[i] is an integer array of nodes adjacent to node i, meaning there is an edge from node i to each node in graph[i].
 
 A node is a terminal node if there are no outgoing edges. A node is a safe node if every possible path starting from that node leads to a terminal node (or another safe node).
