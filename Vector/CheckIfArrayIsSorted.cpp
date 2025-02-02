7016ca5e42e0d4900e914e073831cbecf73cd79f
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-02 21:31:39 +0530

    Update CheckIfArrayIsSorted.cpp

--- Vector/CheckIfArrayIsSorted.cpp
+++ Vector/CheckIfArrayIsSorted.cpp
@@ -3,7 +3,6 @@ Check if Array Is Sorted and Rotated
 Solved
 Easy
 Topics
-Companies
 Hint
 Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
 
