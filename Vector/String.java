3f1800700d8f54f74e00e85c3e96fa71e639e391
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-03 17:05:37 +0530

    Create String.java

---
+++ Vector/String.java
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		String str="java Program";
+		int l=str.length();
+		System.out.println(l);
+		
+		String str2=str.toUpperCase();
+		System.out.println(str2);
+		
+		str2=str.trim();
+		System.out.println(str2);
+		
+		str2=str.substring(3,7);
+		
+		System.out.println(str2);
+		
+		String str5="Pritish kumar ray";
+		System.out.println(str5.startsWith("P"));
+		System.out.println(str5.startsWith("P",6));
+		System.out.println(str5.endsWith("ray"));
+		
+		
+		for(int i=0;i<=str5.length();i++){
+	//	System.out.print(str5.charAt(i));
+		}
+		
+		String str6="www.udemy.com";
+		System.out.println(str6.indexOf("udemy"));
+		System.out.println(str6.indexof)
+		
+	}
+}
