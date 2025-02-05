4489834812412a5b08f922c4978b3d78e2221dcc
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-05 10:40:32 +0530

    ee

--- Vector/java/RotatedTriangle.cpp
+++ Vector/java/RotatedTriangle.cpp
@@ -7,8 +7,9 @@ public class Main {
         System.out.println(); // Newline for formatting
         int count=0;
         for(int i=1;i<=5;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(j+" ");//here instated of j you can change * and count also;
+            for(int j=1;j<=5-i+1;j++){
+                count++;
+                System.out.print("*"+" ");
     
         }
         System.out.println("");
