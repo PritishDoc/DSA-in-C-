55b87c7397165fab33142a38b881db317fc047c8
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-05 10:22:33 +0530

    Create PrintingTriangle.java

---
+++ Vector/java/PrintingTriangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.println(); // Newline for formatting
+        int count=0;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+    
+        }
+        System.out.println("");
+    }
+}}
