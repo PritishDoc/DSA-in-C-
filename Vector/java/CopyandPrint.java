2b5ab95710ff9742e1ef238377b5bd5bd9232ee3
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-05 19:54:50 +0530

    Create CopyandPrint.java

---
+++ Vector/java/CopyandPrint.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+   int arr[]=new int[5];
+   
+   int arr2[]={22,34,55,66};
+   System.out.println(arr2.length);
+   
+   for(int x:arr2){
+       System.out.println(x);
+   }
+}}
