5ba24ff4041db44bc7ded47a950f5e6acfa77aef
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-05 08:31:17 +0530

    Create GpSeries.java

---
+++ Vector/java/GpSeries.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a num: ");
+        int num=sc.nextInt();
+        System.out.println("Enter the distance you want: ");
+        int d=sc.nextInt();
+        System.out.println("Enter the how many terms you want: ");
+        int n=sc.nextInt();
+        int term =num;
+        for(int i=0;i<=n;i++){
+            System.out.print(term+",");
+            term=term*d;
+        }
+        
+        System.out.println(); // Newline for formatting
+    }
+}
