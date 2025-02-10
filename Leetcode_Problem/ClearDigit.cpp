56cb5ea0e14414c03ca039cacf846f142aa11278
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-10 07:36:58 +0530

    Update ClearDigit.cpp

--- Leetcode_Problem/ClearDigit.cpp
+++ Leetcode_Problem/ClearDigit.cpp
@@ -45,3 +45,30 @@ s consists only of lowercase English letters and digits.
 The input is generated such that it is possible to delete all digits.
 
 */
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+            if (isdigit(c)) {
+                if (!st.empty()) {
+                    st.pop(); // Remove closest non-digit character
+                }
+            } else {
+                st.push(c); // Push non-digit characters
+            }
+        }
+
+        // Convert stack to string
+        string result;
+        while (!st.empty()) {
+            result += st.top();
+            st.pop();
+        }
+
+        // Reverse the result since stack stores in LIFO order
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
