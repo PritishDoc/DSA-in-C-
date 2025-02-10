4dcd6d2807dffe23fc9ccfb5958c9289d6a5a26a
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-10 07:43:27 +0530

    Create KsumPath.cpp

---
+++ Leetcode_Problem/KsumPath.cpp
@@ -0,0 +1,30 @@
+
+/*
+
+K Sum Paths
+Difficulty: MediumAccuracy: 44.73%Submissions: 106K+Points: 4
+Given a binary tree and an integer k, determine the number of downward-only paths where the sum of the node values in the path equals k. A path can start and end at any node within the tree but must always move downward (from parent to child).
+
+Examples:
+
+Input: k = 7   
+
+Output: 3
+Explanation: The following paths sum to k 
+ 
+Input: k = 3
+   1
+  /  \
+2     3
+Output: 2
+Explanation:
+Path 1 : 1 -> 2 (Sum = 3)
+Path 2 : 3 (Sum = 3)
+Constraints:
+
+1 ≤ number of nodes ≤ 104
+-100 ≤ node value ≤ 100
+-109 ≤ k ≤ 109
+
+
+*/
