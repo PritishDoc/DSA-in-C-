8d2b31d3dc26f870fe816306237fa662ca231eb0
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-11 23:03:18 +0530

    Update occuraceremove.cpp

--- Leetcode_Problem/occuraceremove.cpp
+++ Leetcode_Problem/occuraceremove.cpp
@@ -1,7 +1,6 @@
 /*
 
  Remove All Occurrences of a Substring
-Medium
 
 Hint
 Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
