3659a092908cea1daddc5cd157100b6699715af6
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-13 21:29:14 +0530

    Create Bst.cpp

---
+++ Leetcode_Problem/Bst.cpp
@@ -0,0 +1,21 @@
+/*
+Pair Sum in BST
+Difficulty: MediumAccuracy: 44.02%Submissions: 72K+Points: 4Average Time: 40m
+Given a Binary Search Tree(BST) and a target. Check whether there's a pair of Nodes in the BST with value summing up to the target. 
+
+Examples:
+
+Input: root = [7, 3, 8, 2, 4, N, 9], target = 12
+       bst
+Output: True
+Explanation: In the binary tree above, there are two nodes (8 and 4) that add up to 12.
+Input: root = [9, 5, 10, 2, 6, N, 12], target = 23
+          bst-3
+Output: False
+Explanation: In the binary tree above, there are no such two nodes exists that add up to 23.
+Constraints:
+
+1 ≤ Number of Nodes ≤ 105
+1 ≤ target ≤ 106
+
+*/
