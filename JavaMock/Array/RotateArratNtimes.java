f3c838e7a2fb3d2e3b8e7e10dae9a88687c2f99d
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-15 20:00:01 +0530

    rotate

--- JavaMock/Array/RotateArratNtimes.java
+++ JavaMock/Array/RotateArratNtimes.java
@@ -2,9 +2,25 @@ package Array;
 
 public class RotateArratNtimes {
     public static void main(String[] args) {
-        int arr[]={
-            
+        int arr[]={1,2,3,4,5,6};
+            int n=3;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<arr.length-1;j++){
+                    int st=0,end=arr.length-1;
+                    if(st<end){
+                        int temp=arr[st];
+                         arr[st]=arr[end];
+                         arr[end]=temp;
+                         st++;
+                         end--;
+                    }
+                }
+            }
+            for(int x:arr){
+                System.out.print(x+" ");
+            }
+
         }
     }
     
-}
+
