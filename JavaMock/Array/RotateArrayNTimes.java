f7a9ff2ee95ea20af08c8df675dca3ce3993c554
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-15 20:21:35 +0530

    l

--- JavaMock/Array/RotateArrayNTimes.java
+++ JavaMock/Array/RotateArrayNTimes.java
@@ -3,7 +3,7 @@ package Array;
 public class RotateArrayNTimes {
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 5, 6};  // Original Array
-        int n = 1;  // Number of rotations
+        int n = 2;  // Number of rotations
 
         // Reduce n if it's greater than array length
         n = n % arr.length;
