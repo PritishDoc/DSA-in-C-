c571a86d54daf1c9c201f564fd50982ed443cbe4
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-16 15:55:34 +0530

    Update LeftShipt.java

--- JavaMock/Array/LeftShipt.java
+++ JavaMock/Array/LeftShipt.java
@@ -5,7 +5,13 @@ public class LeftShipt {
         int arr[]={3,2,4,1,7};
         
 
-        
-        
+        int temp=arr[arr.length-1];
+        for(int i=arr.length-1;i>0;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[0]=temp;
+        for(int x:arr){
+            System.out.print(x+" ");
+        }
     }
 }
