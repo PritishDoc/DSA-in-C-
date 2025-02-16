7c2d0635a3a1efd4d3d741f716c6779580b0dd5f
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-16 16:47:33 +0530

    Update SerializeAndDeserialize.cpp

--- Leetcode_Problem/SerializeAndDeserialize.cpp
+++ Leetcode_Problem/SerializeAndDeserialize.cpp
@@ -23,3 +23,100 @@ Constraints:
 
 
 */
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+class Solution {
+  public:
+    // Function to serialize a tree and return a list containing nodes of tree.
+    vector<int> serialize(Node* root) {
+        vector<int> result;
+        if (!root) return result;
+
+        queue<Node*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            Node* node = q.front();
+            q.pop();
+
+            if (node) {
+                result.push_back(node->data);
+                q.push(node->left);
+                q.push(node->right);
+            } else {
+                result.push_back(-1); // Using -1 to represent NULL
+            }
+        }
+        return result;
+    }
+
+    // Function to deserialize a list and construct the tree.
+    Node* deSerialize(vector<int>& arr) {
+        if (arr.empty()) return NULL;
+
+        queue<Node*> q;
+        Node* root = new Node(arr[0]);
+        q.push(root);
+        int i = 1;
+
+        while (!q.empty() && i < arr.size()) {
+            Node* curr = q.front();
+            q.pop();
+
+            if (arr[i] != -1) {
+                curr->left = new Node(arr[i]);
+                q.push(curr->left);
+            }
+            i++;
+
+            if (i < arr.size() && arr[i] != -1) {
+                curr->right = new Node(arr[i]);
+                q.push(curr->right);
+            }
+            i++;
+        }
+        return root;
+    }
+};
+
+// Helper function for inorder traversal
+void inorder(Node* root, vector<int>& res) {
+    if (!root) return;
+    inorder(root->left, res);
+    res.push_back(root->data);
+    inorder(root->right, res);
+}
+
+int main() {
+    Solution sol;
+
+    // Example tree: [10, 20, 30, 40, 60, N, N]
+    Node* root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->left->left = new Node(40);
+    root->left->right = new Node(60);
+
+    vector<int> serializedTree = sol.serialize(root);
+    Node* deserializedTree = sol.deSerialize(serializedTree);
+
+    vector<int> inorderResult;
+    inorder(deserializedTree, inorderResult);
+
+    for (int val : inorderResult) {
+        cout << val << " ";
+    }
+    return 0;
+}
