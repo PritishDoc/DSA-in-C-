d5afbb93c62ba8569b99cf2b2ece6c13105099c7
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-18 08:56:58 +0530

    Update KClosestPointsToOrigin.cpp

--- Leetcode_Problem/KClosestPointsToOrigin.cpp
+++ Leetcode_Problem/KClosestPointsToOrigin.cpp
@@ -34,3 +34,33 @@ Constraints:
 
 
 */
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int, vector<int>>> maxHeap;
+        
+        for (auto& point : points) {
+            int x = point[0], y = point[1];
+            int dist = x * x + y * y;  // Squared Euclidean distance
+            
+            maxHeap.push({dist, point});
+            
+            if (maxHeap.size() > k) {
+                maxHeap.pop();  // Remove the farthest point
+            }
+        }
+
+        vector<vector<int>> result;
+        while (!maxHeap.empty()) {
+            result.push_back(maxHeap.top().second);
+            maxHeap.pop();
+        }
+        
+        return result;
+    }
+};
