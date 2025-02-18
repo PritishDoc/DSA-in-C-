de544cab0b08d3bb469e93bbd33e500e60ac7b97
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-18 08:33:23 +0530

    Update ConstructSmallestNum.cpp

--- Leetcode_Problem/ConstructSmallestNum.cpp
+++ Leetcode_Problem/ConstructSmallestNum.cpp
@@ -2,7 +2,7 @@
 Construct Smallest Number From DI String
 Medium
 Topics
-Companies
+
 Hint
 You are given a 0-indexed string pattern of length n consisting of the characters 'I' meaning increasing and 'D' meaning decreasing.
 
