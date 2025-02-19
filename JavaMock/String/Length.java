355932cf5ca9bc3a71e22bb29dbdedc529ec2faa
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-19 09:51:23 +0530

    Create Length.java

---
+++ JavaMock/String/Length.java
@@ -0,0 +1,11 @@
+package String;
+
+public class Length {
+    public static void main(String[] args) {
+        String s1="Java";
+        System.out.println(s1.length());
+        System.out.println(s1.charAt(1));
+        System.out.println(s1.charAt(-1));
+    }
+    
+}
