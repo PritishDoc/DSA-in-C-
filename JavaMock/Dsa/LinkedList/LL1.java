9f9fa8801de8e9e292d39b1bdd90abd5baf7901d
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-22 07:28:39 +0530

    Create LL1.java

---
+++ JavaMock/Dsa/LinkedList/LL1.java
@@ -0,0 +1,19 @@
+public class LL1{
+    class Node{
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    //add - First,Last
+    
+
+    public static void main(String[] args) {
+        LL1 list=new LL1();
+
+    }
+    
+}
