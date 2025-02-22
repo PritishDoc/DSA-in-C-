14e17facf4a006a6162c57dd89a349e9a9818d35
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-22 07:45:20 +0530

    Update LL1.java

--- JavaMock/Dsa/LinkedList/LL1.java
+++ JavaMock/Dsa/LinkedList/LL1.java
@@ -1,4 +1,5 @@
 public class LL1{
+    Node head;
     class Node{
         String data;
         Node next;
@@ -9,10 +10,47 @@ public class LL1{
         }
     }
     //add - First,Last
-    
-
+    public void addFirst(String data){
+       Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    //addLast
+    public void addLast(String data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        Node currNode= head;
+        while(currNode.next!=null){
+            currNode=currNode.next;
+        }
+        currNode.next=newNode;
+    }
+    public void PrintList(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode =head;
+        while(currNode!=null){
+            System.out.print(currNode.data+"->");
+            currNode=currNode.next;
+        }
+        System.out.print("null");
+    }
     public static void main(String[] args) {
         LL1 list=new LL1();
+        list.addFirst("A");
+        list.addFirst("B");
+        list.addLast("c");
+        list.PrintList();
+
 
     }
     
