b584086ffd0872849154e7f8fc8f48950a0c2249
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-23 06:07:39 +0530

    Create AutoMorphicNum.java

---
+++ JavaMock/Number/String/Number/java/AutoMorphicNum.java
@@ -0,0 +1,25 @@
+public class AutoMorphicNum {
+    public static void main(String[] args) {
+        int n=3;
+        int sq=n*n;
+        boolean flag=true;
+        while(n>0){
+            if(n%10 !=sq%10){
+                flag=false;
+                break;
+            }
+                //System.out.println("Not a automorphic num");
+                n/=10;
+            sq/=10;
+            
+            
+        }
+        if(flag){
+            System.out.println("Its a automorphic num");
+        }
+        else{
+            System.out.println("Its not an automorphic num");
+        }
+    }
+    
+}
