366686b0611cc256b31edb2122a16a789fee084c
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-24 10:08:20 +0530

    Update DiamondPattern.java

--- JavaMock/PatternProblem/DiamondPattern.java
+++ JavaMock/PatternProblem/DiamondPattern.java
@@ -2,6 +2,25 @@ package PatternProblem;
 
 public class DiamondPattern {
     public static void main(String[] args) {
+        int row=5;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<row-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=0;i<row-1;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<row-i-1;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
         
     }
     
