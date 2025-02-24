e449762b125ae9490a9ba042139a2971279c52e7
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-24 19:03:48 +0530

    Update MergedTwoSortedList.java

--- JavaMock/Dsa/LinkedList/MergedTwoSortedList.java
+++ JavaMock/Dsa/LinkedList/MergedTwoSortedList.java
@@ -35,3 +35,43 @@ The number of nodes in both lists is in the range [0, 50].
 -100 <= Node.val <= 100
 Both list1 and list2 are sorted in non-decreasing order.
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy=new ListNode(0);
+        ListNode current=dummy;
+        while(list1!=null && list2!=null){
+            if(list1.val<list2.val){
+                current.next=list1;
+                list1=list1.next;
+            }
+            else{
+                current.next=list2;
+                list2=list2.next;
+            }
+            current=current.next;
+        }
+            //if some element left
+            if(list1!=null){
+                current.next=list1;
+            }
+            else if(list2!=null){
+                current.next=list2;
+            }
+            
+         
+         
+
+        return dummy.next;
+        
+    }
+}
