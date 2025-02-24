74fd29c435ecbc211b1d6afd8de7c65a77093396
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-24 11:46:11 +0530

    Update addLinkedList.java

--- JavaMock/Dsa/LinkedList/addLinkedList.java
+++ JavaMock/Dsa/LinkedList/addLinkedList.java
@@ -34,3 +34,47 @@ The number of nodes in each linked list is in the range [1, 100].
 0 <= Node.val <= 9
 It is guaranteed that the list represents a number that does not have leading zeros.
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0); 
+        ListNode current = dummy; 
+        int carry = 0; 
+
+        while (l1 != null || l2 != null) {
+            int sum = carry; 
+
+            if (l1 != null) { 
+                sum += l1.val;
+                l1 = l1.next; 
+            }
+
+            if (l2 != null) { 
+                sum += l2.val;
+                l2 = l2.next; 
+            }
+
+            carry = sum / 10; 
+            int digit = sum % 10; 
+
+            current.next = new ListNode(digit); // Store the computed digit
+            current = current.next; // Move the pointer forward
+        }
+
+        
+        if (carry > 0) {
+            current.next = new ListNode(carry);
+        }
+
+        return dummy.next; 
+    }
+}
