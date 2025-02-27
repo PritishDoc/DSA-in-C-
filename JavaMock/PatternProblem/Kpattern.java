9318f8b6b43e036e7d523d63655de458dcc2fc70
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-27 08:54:41 +0530

    Create Kpattern.java

---
+++ JavaMock/PatternProblem/Kpattern.java
@@ -0,0 +1,20 @@
+package PatternProblem;
+
+public class Kpattern {
+    public static void main(String[] args) {
+        int n=6;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(j==1||(i+j==5)||i+j>=6){
+                    System.out.print("*");
+                }
+                else{
+                System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}
