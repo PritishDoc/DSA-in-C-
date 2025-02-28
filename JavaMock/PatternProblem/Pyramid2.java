f4ca6cdd39bf897a8791a7bb6dcb92933ed44975
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-28 09:59:21 +0530

    Create Pyramid2.java

---
+++ JavaMock/PatternProblem/Pyramid2.java
@@ -0,0 +1,30 @@
+package PatternProblem;
+
+public class Pyramid2 {
+   
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print increasing numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            // Print decreasing numbers
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            // Move to next line
+            System.out.println();
+        }
+    }
+}
+
+
