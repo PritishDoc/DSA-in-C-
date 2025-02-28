e4ee8e59d1a64a454eaf32a0e17686dc8dbc2abd
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-02-28 09:48:01 +0530

    Update Pyramid.java

--- JavaMock/PatternProblem/Pyramid.java
+++ JavaMock/PatternProblem/Pyramid.java
@@ -1,5 +1,28 @@
 package PatternProblem;
 
 public class Pyramid {
-    
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+        int sp = n - 1; // Initial spaces
+        int st = 1; // Initial stars
+
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = 1; j <= sp; j++) {
+                System.out.print(" ");
+            }
+            
+            // Print stars
+            for (int j = 1; j <= st; j++) {
+                System.out.print("*");
+            }
+            
+            // Move to next line
+            System.out.println();
+            
+            // Decrease spaces and increase stars
+            sp--;
+            st += 2;
+        }
+    }
 }
