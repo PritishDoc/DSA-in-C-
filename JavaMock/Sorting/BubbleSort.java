eb5d0b36cdf72e223675af96068e454339969e39
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-04-04 06:21:04 +0530

    Update BubbleSort.java

--- JavaMock/Sorting/BubbleSort.java
+++ JavaMock/Sorting/BubbleSort.java
@@ -8,6 +8,7 @@ public class BubbleSort{
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    isswap = true; // update swap flag
                 }
             }
             if(!isswap){
