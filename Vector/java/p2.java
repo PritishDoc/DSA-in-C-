fd1b7173e0215ce54db957099a5ad3ac45beab75
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-05-15 21:32:57 +0530

    Update p2.java

--- Vector/java/p2.java
+++ Vector/java/p2.java
@@ -3,21 +3,9 @@ public class p2 {
      //reverse num
     //automorphic
     //neon num
-    int n=9;
-    int sq=n*n;
-    int sum=0;
-    while(sq>0){
-         sum+=sq%10;
-        
-        n/=10;
-    }
-    if(sum==n){
-        System.out.println("automorphic");
-    }
-    else{
-        System.out.println("not");
-    }
-    }
+    //spy num
+        int n=123;
       
     }
+}
 
