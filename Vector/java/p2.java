bcd176013068b838b53f2a459e92aa31afde28e5
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-05-15 20:44:06 +0530

    Update p2.java

--- Vector/java/p2.java
+++ Vector/java/p2.java
@@ -2,21 +2,14 @@ public class p2 {
     public static void main(String[] args) {
      //reverse num
     //automorphic
-
-    int n=55;
+    //neon num
+    int n=9;
     int sq=n*n;
-    boolean flag =true;
-    if(sq%10!=n%10)
-    {
-        flag=false;
+    int sum=0;
+    while(sq>0){
+        int 
     }
-    sq/=10;
-    n/=10;
-        if(flag){
-       System.out.println("yes"); 
-     }  else{
-        System.out.println("not");
-     }
+    
     }
       
     }
