f3530e8ce1dc3b8072c88db131ebe9626f9225fe
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-05-15 21:48:57 +0530

    Update p2.java

--- Vector/java/p2.java
+++ Vector/java/p2.java
@@ -5,7 +5,21 @@ public class p2 {
     //neon num
     //spy num
         int n=123;
-      
+        int sum=0;
+        int mul=0;
+
+        while(n>0){
+            sum+=n%10;
+            mul+=n%10;
+            n/=10;
+
+        }
+      if(sum==mul){
+        System.out.println("Perfectno");
+      }
+      else{
+        System.out.println("Not perfect");
+      }
     }
 }
 
