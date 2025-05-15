31feb4f114ab07b1336c9a8b05bc395e1d3b317e
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-05-15 20:18:40 +0530

    Update p2.java

--- Vector/java/p2.java
+++ Vector/java/p2.java
@@ -4,10 +4,12 @@ public class p2 {
         int n=2345;
         int rev=0;
         while(n>0){
-            
+            int rem=n%10;
+            rev=rev*10+rem;
+            n/=10;
         }
-    
+       System.out.println(rev); 
      }  
-         System.out.println(rev); 
+      
     }
-}
+
