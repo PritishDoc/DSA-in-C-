f67e0635b49d0a1faaf523383985cdc1f3287731
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-05-15 20:24:15 +0530

    Update p2.java

--- Vector/java/p2.java
+++ Vector/java/p2.java
@@ -1,14 +1,15 @@
 public class p2 {
     public static void main(String[] args) {
      //reverse num
-        int n=2345;
-        int rev=0;
+        int n=25;
+        int count=0;
         while(n>0){
-            int rem=n%10;
-            rev=rev*10+rem;
             n/=10;
+            count++;
         }
-       System.out.println(rev); 
+        int sq=n*n;
+        if()
+       System.out.println(count); 
      }  
       
     }
